cf4d10ab74c74d5e4da323e8f0feda3f624f9a4b
camsc <cam@Camerons-MacBook-Air.local>
2014-08-09 13:03:47 -0700

    updates!
    updates

---
+++ standard-types.js
@@ -0,0 +1,54 @@
+var standardTypes = {
+	"skill": {
+		description: "Skill",
+		iconClass: "fa-gamepad"
+		},
+	"understanding": {
+		description: "Build understanding",
+		iconClass: "fa-cloud"
+		},
+	"new-concept": {
+		description: "New concept",
+		iconClass: "fa-lightbulb-o"
+		},
+	"variety-of-methods": {
+		description: "Employ a variety of methods",
+		iconClass: "fa-briefcase"
+		},
+	"standard-algorithm": {
+		description: "Employ the standard algorithm",
+		iconClass: "fa-wrench"
+		},
+	"capstone": {
+		description: "Capstone problems",
+		iconClass: "fa-graduation-cap"
+		},
+	"fluency": {
+		description: "Compute fluently",
+		iconClass: "fa-flash"
+		},
+	"real-world": {
+		description: "Real-world application",
+		iconClass: "fa-globe"
+		},
+	"link": {
+		description: "Link concepts",
+		iconClass: "fa-link"
+		},
+	"compare": {
+		description: "Compare or sort",
+		iconClass: "fa-filter"
+		},
+	"verify": {
+		description: "Verify",
+		iconClass: "fa-flask"
+		},
+	"justify": {
+		description: "Justify or explain",
+		iconClass: "fa-gavel"
+		},
+	"prove": {
+		description: "Prove",
+		iconClass: "fa-bank"
+		}
+};
